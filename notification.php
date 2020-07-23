dc8e1ee60d72a483ebc67685056c5d1a086666c5
Martin <martin.a.chaparro@gmail.com>
2020-07-23 02:16:37 -0300

    Archivos de Notificaciones y url de retornos

--- notification.php
+++ notification.php
@@ -1,1 +1,7 @@
 <?php
+
+$logger1  = $_GET.PHP_EOL;
+$logger2  = $_GET.PHP_EOL;
+file_put_contents('./log_1_'.date("j.n.Y").'.log', $logger1, FILE_APPEND);
+file_put_contents('./log_2_'.date("j.n.Y").'.log', $logger2, FILE_APPEND);
+
