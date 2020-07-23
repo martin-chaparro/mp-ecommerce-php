caf3704a8fec12e411c949eadec6d55db8eb0dd3
Martin <martin.a.chaparro@gmail.com>
2020-07-23 04:32:55 -0300

    Lo guardo en el error log

--- notification.php
+++ notification.php
@@ -1,6 +1,7 @@
 <?php
 
 error_log("===========  POST  ============== ".print_r($_POST, true));
+error_log("===========  GET  ============== ".print_r($_GET, true));
 error_log("===========  REQUEST  ============== ".print_r($_REQUEST, true));
 $entityBody = file_get_contents('php://input');
 error_log("===========  TODO  ============== ".print_r($entityBody, true));
