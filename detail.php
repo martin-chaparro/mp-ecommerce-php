15cd321fe1f675e13966fd0c5f3d4fbbdd9c0103
Martin <martin.a.chaparro@gmail.com>
2020-07-23 09:32:21 -0300

    modifico boton de pago

--- detail.php
+++ detail.php
@@ -45,7 +45,7 @@
         .mp-spinner-path { stroke-dasharray: 1, 200; stroke-dashoffset: 0; animation: loading-dash 1.5s ease-in-out infinite; stroke-linecap: round; stroke-width: 2px; stroke: #009ee3; }
     </style>
     <style type="text/css">
-        .mercadopago-button { padding: 0 1.7142857142857142em; font-family: "Helvetica Neue", Arial, sans-serif; font-size: 0.875em; line-height: 2.7142857142857144; background: #009ee3; border-radius: 0.2857142857142857em; color: #fff; cursor: pointer; border: 0; di }
+        .mercadopago-button { padding: 14px; font-family: "Helvetica Neue", Arial, sans-serif; font-size: 0.875em; line-height: 2.7142857142857144; background: #009ee3; border-radius: 0.2857142857142857em; color: #fff; cursor: pointer; border: 0; di }
     </style>
     <style type="text/css">
         @keyframes loading-rotate { 100% { transform: rotate(360deg); } }
