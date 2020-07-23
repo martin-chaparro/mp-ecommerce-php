2446284a4929db56f5e8e4d876954c3b3086ccf8
Martin <martin.a.chaparro@gmail.com>
2020-07-23 02:55:35 -0300

    modifico el boton

--- detail.php
+++ detail.php
@@ -223,7 +223,8 @@
                                     $preference->save();
 
                                     ?>
-                                    <a href="<?php echo $preference->init_point; ?>">Pagar la compra</a>
+                                    <a href="<?php echo $preference->init_point; ?>" class="mercadopago-button">Pagar la compra</a>
+<!--                                    <button type="submit" class="mercadopago-button" formmethod="post">Pagar</button>-->
                                 </div>
                             </div>
                         </div>
